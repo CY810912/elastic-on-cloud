ae7392ec7f9a13aada4a41e5d03aa30e6d8f6f56
jacky <dschengyang@gmail.com>
2020-09-24 22:23:54 +0800

    pom fix

--- src/test/java/com/ithelp/ironman/ecdemo/EcDemoApplicationTests.java
+++ src/test/java/com/ithelp/ironman/ecdemo/EcDemoApplicationTests.java
@@ -1,8 +1,12 @@
 package com.ithelp.ironman.ecdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EcDemoApplicationTests {
 
