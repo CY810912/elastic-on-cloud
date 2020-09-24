82f0983643c28c5930cd57a13a1ddc0bf31ec006
jacky <dschengyang@gmail.com>
2020-09-24 20:10:13 +0800

    ec demo init

---
+++ src/main/java/com/ithelp/ironman/ecdemo/EcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ithelp.ironman.ecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ithelp/ironman/ecdemo/EcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithelp.ironman.ecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
