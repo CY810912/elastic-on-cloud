3c745907dfefcf934c394384e2f87596242e8a72
jacky <dschengyang@gmail.com>
2020-09-25 22:36:10 +0800

    update

---
+++ src/main/java/com/ithelp/ironman/ecdemo/bean/es/Profile.java
@@ -0,0 +1,11 @@
+package com.ithelp.ironman.ecdemo.bean.es;
+
+import lombok.Data;
+
+@Data
+public class Profile {
+    private String id;
+    private String name;
+    private String six;
+    private String phone;
+}
---
+++ src/main/java/com/ithelp/ironman/ecdemo/constant/EsConstant.java
@@ -0,0 +1,7 @@
+package com.ithelp.ironman.ecdemo.constant;
+
+public class EsConstant {
+    public static final String INDEX="it_help";
+    public static final String TYPE="_doc";
+
+}
